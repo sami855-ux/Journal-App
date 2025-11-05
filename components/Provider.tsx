c91abe2fadd5e5aaf5318f8a053a700f32efa364
sami855-ux <samitale56@outlook.com>
2025-11-05 21:53:15 +0300

    clerk is adedd to the project

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,32 +1,39 @@
-import { useColorScheme } from 'react-native'
-import { TamaguiProvider, type TamaguiProviderProps } from 'tamagui'
-import { ToastProvider, ToastViewport } from '@tamagui/toast'
-import { CurrentToast } from './CurrentToast'
-import { config } from '../tamagui.config'
+import { useColorScheme } from "react-native";
+import { TamaguiProvider, type TamaguiProviderProps } from "tamagui";
+import { ToastProvider, ToastViewport } from "@tamagui/toast";
+import { CurrentToast } from "./CurrentToast";
+import { config } from "../tamagui.config";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
-export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
-  const colorScheme = useColorScheme()
+export function Provider({
+  children,
+  ...rest
+}: Omit<TamaguiProviderProps, "config">) {
+  const colorScheme = useColorScheme();
 
   return (
-    <TamaguiProvider
-      config={config}
-      defaultTheme={colorScheme === 'dark' ? 'dark' : 'light'}
-      {...rest}
-    >
-      <ToastProvider
-        swipeDirection="horizontal"
-        duration={6000}
-        native={
-          [
-            // uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go
-            // 'mobile'
-          ]
-        }
+    <ClerkProvider tokenCache={tokenCache}>
+      <TamaguiProvider
+        config={config}
+        defaultTheme={colorScheme === "dark" ? "dark" : "light"}
+        {...rest}
       >
-        {children}
-        <CurrentToast />
-        <ToastViewport top="$8" left={0} right={0} />
-      </ToastProvider>
-    </TamaguiProvider>
-  )
+        <ToastProvider
+          swipeDirection="horizontal"
+          duration={6000}
+          native={
+            [
+              // uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go
+              // 'mobile'
+            ]
+          }
+        >
+          {children}
+          <CurrentToast />
+          <ToastViewport top="$8" left={0} right={0} />
+        </ToastProvider>
+      </TamaguiProvider>
+    </ClerkProvider>
+  );
 }
